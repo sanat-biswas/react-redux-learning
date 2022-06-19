05a03535c4d513467cef5124ee904fd5582d4814
sanat-biswas <sanatbiswas786@gmail.com>
2022-06-19 21:16:03 +0530

    initial commit

---
+++ src/components/user/UserContainer.js
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react';
+import {connect} from 'react-redux'
+import { fetchUsers } from '../../redux';
+
+function UserContainer({userData, fetchUsers}) {
+
+    useEffect(() => {
+        fetchUsers();
+    })
+    return (
+        <div>
+            <h1>UserContainer</h1>
+            {
+                userData.loading ? (
+                    <h2>loading</h2>
+                ) : userData.error ? (
+                    <h2>userData.error</h2>
+                ) : (
+                    <div>
+                        {
+                            userData && userData.users && userData.users.map(user => <p>{user.name}</p>)
+                        }
+                    </div>
+                )
+            }
+        </div>
+    )
+}
+
+const mapStateToProps = (state) => {
+
+    return {
+        userData: state.user
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        fetchUsers: () => dispatch(fetchUsers())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserContainer);
---
+++ src/redux/user/userActions.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { FETCH_USER_FAILURE, FETCH_USER_REQUEST, FETCH_USER_SUCCESS } from "./userTypes";
+
+export const fetchUsersRequest = () => {
+    return {
+        type: FETCH_USER_REQUEST
+    }
+}
+
+export const fetchUsersSuccess = (users) => {
+    return {
+        type: FETCH_USER_SUCCESS,
+        payload: users
+    }
+}
+
+export const fetchUsersFailure = (error) => {
+    return {
+        type: FETCH_USER_FAILURE,
+        payload: error
+    }
+}
+
+export const fetchUsers = () => {
+    return (dispatch) => {
+        dispatch(fetchUsersRequest);
+        axios.get('http://localhost:8000/api/users').then(response => {
+            console.log("response", response);
+            const users = response.data.users;
+            dispatch(fetchUsersSuccess(users));
+        }).catch(error => {
+            const err = error.message;
+            dispatch(fetchUsersFailure(err));
+        })
+    }
+}
